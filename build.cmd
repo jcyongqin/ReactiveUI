db701e1f3fa8a89a3136bd7440a8126d4212bf38
Olly Levett <oliver.levett@softwire.com>
2018-02-22 09:55:33 +0000

    chore: run build script in verbose

--- build.cmd
+++ build.cmd
@@ -3,6 +3,6 @@ tools\nuget\nuget.exe update -self
 tools\nuget\nuget.exe install xunit.runner.console -OutputDirectory tools -Version 2.2.0 -ExcludeVersion
 tools\nuget\nuget.exe install Cake -OutputDirectory tools -ExcludeVersion -Version 0.21.1
 
-tools\Cake\Cake.exe build.cake --target=%1
+tools\Cake\Cake.exe build.cake --target=%1 -verbosity=Diagnostic
 
 exit /b %errorlevel%
